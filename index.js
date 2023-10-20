a74ca89d9f895e4cea0a40f3f27b2793e3ed7f74
moses <moses.kingi@moringaschool.com>
2023-10-20 22:00:08 +0300

    completed assigment redone

--- index.js
+++ index.js
@@ -1,59 +1,169 @@
-> const cats  = ["Milo", "Otis", "Garfield"];
-undefined
-> cats
-[ 'Milo', 'Otis', 'Garfield' ]
-> cats.push("Ralph");
-4
-> cats
-[ 'Milo', 'Otis', 'Garfield', 'Ralph' ]
-> cats.unshift("Bob");
-5
-> cats
-[ 'Bob', 'Milo', 'Otis', 'Garfield', 'Ralph' ]
-> cats.pop("Ralph");
-'Ralph'
-> cats
-[ 'Bob', 'Milo', 'Otis', 'Garfield' ]
-> cats.slice(1,-1);
-[ 'Milo', 'Otis' ]
-> cats
-[ 'Bob', 'Milo', 'Otis', 'Garfield' ]
-> cats.slice(2)
-[ 'Otis', 'Garfield' ]
-> cats.push("Broom");
-5
-> cats
-[ 'Bob', 'Milo', 'Otis', 'Garfield', 'Broom' ]
-> cats.slice(1,length-1);
-Uncaught ReferenceError: length is not defined
-> cats.slice(1,-1);
-[ 'Milo', 'Otis', 'Garfield' ]
-> cats.unshift("Arnold");
-6
-> cats
-[ 'Arnold', 'Bob', 'Milo', 'Otis', 'Garfield', 'Broom' ]
-> cats.slice(2,-1);
-[ 'Milo', 'Otis', 'Garfield' ]
-> cats.pop(1,);
-'Broom'
-> cats
-[ 'Arnold', 'Bob', 'Milo', 'Otis', 'Garfield' ]
-> cats.slice(2,-1);
-[ 'Milo', 'Otis' ]
-> cats
-[ 'Arnold', 'Bob', 'Milo', 'Otis', 'Garfield' ]
-> cats.slice(2,);
-[ 'Milo', 'Otis', 'Garfield' ]
-> cats
-[ 'Arnold', 'Bob', 'Milo', 'Otis', 'Garfield' ]
-> cats.slice(3);
-[ 'Otis', 'Garfield' ]
-> cats
-[ 'Arnold', 'Bob', 'Milo', 'Otis', 'Garfield' ]
-> cats.slice(2,);
-[ 'Milo', 'Otis', 'Garfield' ]
+
+const cats = function(){
+    console.log('["Milo", "Otis", "Garfield"]')
+}
+
+function destructivelyAppendCat(name){
+
+    beforeEach(function () {
+        cats.length = 0;
+      
+        cats.push("Milo", "Otis", "Garfield");
+      })
+      
+        cats.push("Ralph");
+    
+};
+      
+destructivelyAppendCat();
+
+
+function destructivelyPrependCat(name){
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.unshift("Bob");
+  })
+}
+
+destructivelyPrependCat();
+
+ 
+function destructivelyRemoveLastCat(name){
+    beforeEach(function () {
+        cats.length = 0;
+      
+        cats.pop();
+      })
+    }
+    
+    destructivelyRemoveLastCat();
+
+
+    
+function destructivelyRemoveFirstCat(name){
+    beforeEach(function () {
+        cats.length = 0;
+      
+        cats.shift();
+      })
+    }
+    
+    destructivelyRemoveFirstCat();
 
 
 
 
 
+    function appendCat(name){
+
+        beforeEach(function () {
+            cats.length = 0;
+
+     const cats = ["Milo", "Otis", "Garfield"]; 
+      copyOfCats=cats.slice();
+            cats.push("Broom")
+         
+        })
+
+    }
+         appendCat();
+
+
+
+   
+    function prependCat(name){
+
+        beforeEach(function () {
+            cats.length = 0;
+
+     const cats = ["Milo", "Otis", "Garfield"]; 
+      copyOfCats=[...cats];
+      cats.shift("Arnold");
+         
+        })
+
+    }
+        prependCat();
+
+
+
+        function removeLastCat(name){
+
+            beforeEach(function () {
+                cats.length = 0;
+    
+         const cats = ["Milo", "Otis", "Garfield"]; 
+          copyOfCats=[...cats];
+                cats.slice(-1);
+             
+            })
+    
+        }
+        removeLastCat();
+    
+
+
+
+        function removeFirstCat(name){
+
+            beforeEach(function () {
+                cats.length = 0;
+    
+         const cats = ["Milo", "Otis", "Garfield"]; 
+          copyOfCats=[...cats];
+                cats.slice(1);
+             
+            })
+    
+        }
+        removeFirstCat();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      function destructivelyAppendCat(name){
+
+        beforeEach(function () {
+            cats.length = 0;
+          
+    
+        });
+    };
+
+    function destructivelyRemoveLastCat(name){
+        beforeEach(function () {
+            cats.length = 0;
+
+    cats.pop.shift();
+          
+           
+});
+
+};
+
+
+function destructivelyFirstLastCat(name){
+
+        beforeEach(function () {
+            cats.length = 0;
+          
+            cats.pop.shift();
+
+    
+        });
+    };
